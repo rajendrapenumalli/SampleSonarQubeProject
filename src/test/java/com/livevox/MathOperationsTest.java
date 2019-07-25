aca697850833c9733531d356df65b9f10f189be6
rpenumalli <rpenumalli@Livevox.onmicrosoft.com>
2019-07-25 15:11:56 +0530

    src/test/java/com/livevox/MathOperationsTest.java

---
+++ src/test/java/com/livevox/MathOperationsTest.java
@@ -0,0 +1,42 @@
+package com.livevox;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+public class MathOperationsTest  {
+	MathOperations cal=null;
+    
+	@Before
+	public void setup() {
+		cal=new MathOperations();
+	}
+	
+	
+	@Test
+	public void testAdd() {
+		int sum=cal.add(1,2);
+		assertTrue(sum==3);
+	}
+
+	@Test
+	public void testMulti() {
+		int prod=cal.multi(1,2);
+		assertTrue(prod==2);
+	}
+
+	@Test
+	public void testSub() {
+		int diff=cal.sub(5,2);
+
+		assertTrue(diff==3);
+	}
+	@Test
+	public void testDiv() {
+		int res=cal.div(5,2);
+		assertTrue(res==2);
+	}
+
+}
+
